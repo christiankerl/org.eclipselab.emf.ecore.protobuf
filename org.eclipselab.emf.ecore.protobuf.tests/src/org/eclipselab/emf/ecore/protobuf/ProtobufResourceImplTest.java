2967ad63c8205aeed670bfc0f15ec7c464d4d3eb
Christian Kerl <christian-kerl@web.de>
2011-05-10 22:23:30 +0200

    added proof of concept implementation of ProtobufResourceImpl

--- org.eclipselab.emf.ecore.protobuf.tests/src/org/eclipselab/emf/ecore/protobuf/ProtobufResourceImplTest.java
+++ org.eclipselab.emf.ecore.protobuf.tests/src/org/eclipselab/emf/ecore/protobuf/ProtobufResourceImplTest.java
@@ -1,5 +1,15 @@
 package org.eclipselab.emf.ecore.protobuf;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipselab.emf.ecore.protobuf.tests.library.Author;
+import org.eclipselab.emf.ecore.protobuf.tests.library.Book;
+import org.eclipselab.emf.ecore.protobuf.tests.library.Library;
+import org.eclipselab.emf.ecore.protobuf.tests.library.LibraryFactory;
+import org.eclipselab.emf.ecore.protobuf.tests.library.LibraryPackage;
+import org.eclipselab.emf.ecore.protobuf.tests.library.util.LibraryResourceFactoryImpl;
 import org.junit.Test;
 
 /**
@@ -9,6 +19,33 @@ public class ProtobufResourceImplTest {
 
 	@Test
 	public void test() throws Exception {
+		ResourceSet resources = new ResourceSetImpl();
+		resources.getPackageRegistry().put(LibraryPackage.eNS_URI, LibraryPackage.eINSTANCE);
+		resources.getResourceFactoryRegistry().getExtensionToFactoryMap().put("library", new LibraryResourceFactoryImpl());
 		
+		Resource resource = resources.createResource(URI.createFileURI("./fixture.library"));
+				
+		LibraryFactory factory = LibraryFactory.eINSTANCE;
+		
+		Author author = factory.createAuthor();
+		author.setName("Me");
+		
+		Book book = factory.createBook();
+		book.setName("Tales of EMF and ProtoBuf");
+		book.setAuthor(author);
+		
+		Library lib = factory.createLibrary();
+		lib.setName("MyVirtualLibrary");
+		// lib.getAuthors().add(author);
+		// lib.getBooks().add(book);
+
+		resource.getContents().add(lib);
+		resource.save(null);
+		
+		resource.unload();
+		
+		resource.load(null);
+		
+		resource.getContents();
 	}
 }
--- org.eclipselab.emf.ecore.protobuf/src/org/eclipselab/emf/ecore/protobuf/ProtobufResourceImpl.java
+++ org.eclipselab.emf.ecore.protobuf/src/org/eclipselab/emf/ecore/protobuf/ProtobufResourceImpl.java
@@ -3,24 +3,144 @@ package org.eclipselab.emf.ecore.protobuf;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+
+import com.google.protobuf.DescriptorProtos;
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto.Type;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.Descriptors.DescriptorValidationException;
+import com.google.protobuf.DynamicMessage;
 
 /**
  * A {@link Resource} implementation providing serialization in Google's Protocol Buffer format.
  */
 public class ProtobufResourceImpl extends ResourceImpl {
 	
+	public ProtobufResourceImpl() {
+		super();
+	}
+
+	public ProtobufResourceImpl(URI uri) {
+		super(uri);
+	}
+
 	@Override
 	protected void doSave(OutputStream outputStream, Map<?, ?> options) throws IOException {
-		// iterate over content
-		// if new epackage map epackage to protobuf description
+		EcoreProtos.EResourceProto.Builder resource = EcoreProtos.EResourceProto.newBuilder();
+		
+		Map<EPackage, EcoreProtos.EPackageProto.Builder> packages = new HashMap<EPackage, EcoreProtos.EPackageProto.Builder>();
+		Map<EClass, EcoreProtos.EClassProto.Builder> classes = new HashMap<EClass, EcoreProtos.EClassProto.Builder>();
+		
+		DescriptorProtos.FileDescriptorProto.Builder protos = DescriptorProtos.FileDescriptorProto.newBuilder();
+		int id = 1;
+		
+		Descriptors.FileDescriptor file = null;
+		
+		for(EObject eObject : getContents()) {
+			EClass eClass = eObject.eClass();
+			
+			if(!packages.containsKey(eClass.getEPackage())) {
+				EcoreProtos.EPackageProto.Builder b = resource.addEPackageBuilder();
+				b.setUri(eClass.getEPackage().getNsURI());
+				
+				packages.put(eClass.getEPackage(), b);
+			}
+			
+			if(!classes.containsKey(eClass)) {
+				EcoreProtos.EClassProto.Builder b = packages.get(eClass.getEPackage()).addEClassBuilder();
+				DescriptorProtos.DescriptorProto.Builder db = protos.addMessageTypeBuilder();
+				
+				int number = 1;
+				
+				db.setName(eClass.getName());
+				
+				for(EAttribute attribute : eClass.getEAllAttributes()) {
+					db.addFieldBuilder()
+						.setName(attribute.getName())
+						.setNumber(number++)
+						.setType(Type.TYPE_STRING);
+				}
+				
+				b.setId(id++);
+				b.setDefinition(db);
+				
+				classes.put(eClass, b);
+				
+				try {
+					file = Descriptors.FileDescriptor.buildFrom(protos.build(), new Descriptors.FileDescriptor[0]);
+				} catch (DescriptorValidationException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			
+			EcoreProtos.EClassProto.Builder c = classes.get(eClass);
+			
+			EcoreProtos.EObjectProto.Builder b = resource.addEObjectBuilder();
+			b.setEClassId(c.getId());
+						
+			Descriptors.Descriptor dc = file.findMessageTypeByName(eClass.getName());
+			
+			DynamicMessage.Builder m = DynamicMessage.newBuilder(dc);
+			
+			for(EAttribute attribute : eClass.getEAllAttributes()) {
+				if(eObject.eIsSet(attribute)) {
+					m.setField(dc.findFieldByName(attribute.getName()), eObject.eGet(attribute));
+				}
+			}
+			
+			b.setData(m.build().toByteString());
+		}
+		
+		outputStream.write(resource.build().toByteArray());
 	}
-	
+		
 	@Override
 	protected void doLoad(InputStream inputStream, Map<?, ?> options) throws IOException {
+		EcoreProtos.EResourceProto resource = EcoreProtos.EResourceProto.parseFrom(inputStream);
 		
+		Map<Integer, EClass> classes = new HashMap<Integer, EClass>();
+		DescriptorProtos.FileDescriptorProto.Builder protos = DescriptorProtos.FileDescriptorProto.newBuilder();
+		
+		for(EcoreProtos.EPackageProto ePackage : resource.getEPackageList()) {
+			EPackage p = getResourceSet().getPackageRegistry().getEPackage(ePackage.getUri());
+			
+			for(EcoreProtos.EClassProto eClass : ePackage.getEClassList()) {
+				classes.put(eClass.getId(), (EClass) p.getEClassifier(eClass.getDefinition().getName()));
+				protos.addMessageType(eClass.getDefinition());
+			}
+		}
+		Descriptors.FileDescriptor file = null;
+		
+		try {
+			file = Descriptors.FileDescriptor.buildFrom(protos.build(), new Descriptors.FileDescriptor[0]);
+		} catch (DescriptorValidationException e) {
+			throw new RuntimeException(e);
+		}
+		
+		for(EcoreProtos.EObjectProto eObject : resource.getEObjectList()) {
+			int id = eObject.getEClassId();
+			
+			EClass eClass = classes.get(id);
+			Descriptors.Descriptor descriptor = file.findMessageTypeByName(eClass.getName());
+			
+			DynamicMessage m = DynamicMessage.parseFrom(descriptor, eObject.getData());
+			
+			EObject obj = eClass.getEPackage().getEFactoryInstance().create(eClass);
+			
+			for(Map.Entry<Descriptors.FieldDescriptor, Object> field : m.getAllFields().entrySet()) {
+				obj.eSet(eClass.getEStructuralFeature(field.getKey().getName()), field.getValue());
+			}
+			
+			getContents().add(obj);
+		}
 	}
 }
