99f079bfbe3cdbe2e4f1a5cfe86c38b284011b01
Christian Kerl <christian-kerl@web.de>
2011-05-26 18:35:26 +0200

    updated ProtobufResourceImplTest to use recently added EEnum

--- org.eclipselab.emf.ecore.protobuf.tests/src/org/eclipselab/emf/ecore/protobuf/ProtobufResourceImplTest.java
+++ org.eclipselab.emf.ecore.protobuf.tests/src/org/eclipselab/emf/ecore/protobuf/ProtobufResourceImplTest.java
@@ -1,5 +1,10 @@
 package org.eclipselab.emf.ecore.protobuf;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -9,6 +14,7 @@ import org.eclipselab.emf.ecore.protobuf.tests.library.Book;
 import org.eclipselab.emf.ecore.protobuf.tests.library.Library;
 import org.eclipselab.emf.ecore.protobuf.tests.library.LibraryFactory;
 import org.eclipselab.emf.ecore.protobuf.tests.library.LibraryPackage;
+import org.eclipselab.emf.ecore.protobuf.tests.library.Rating;
 import org.eclipselab.emf.ecore.protobuf.tests.library.util.LibraryResourceFactoryImpl;
 import org.junit.Test;
 
@@ -33,19 +39,34 @@ public class ProtobufResourceImplTest {
 		Book book = factory.createBook();
 		book.setName("Tales of EMF and ProtoBuf");
 		book.setAuthor(author);
+		book.setRating(Rating.MEDIUM);
 		
 		Library lib = factory.createLibrary();
 		lib.setName("MyVirtualLibrary");
-		// lib.getAuthors().add(author);
-		// lib.getBooks().add(book);
+		lib.getAuthors().add(author);
+		lib.getBooks().add(book);
 
 		resource.getContents().add(lib);
-		resource.save(null);
+		
+		ByteArrayOutputStream dataOutput = new ByteArrayOutputStream(1024);
+		
+		long start = System.nanoTime();
+		resource.save(dataOutput, null);
+		System.out.println((System.nanoTime() - start) / 10.0e5);
 		
 		resource.unload();
 		
-		resource.load(null);
+		ByteArrayInputStream dataInput = new ByteArrayInputStream(dataOutput.toByteArray());
 		
-		resource.getContents();
+		start = System.nanoTime();
+		resource.load(dataInput, null);
+		System.out.println((System.nanoTime() - start) / 10.0e5);
+		
+		lib = (Library) resource.getContents().get(0);
+		
+		System.out.println(lib);
+		System.out.println(lib.getAuthors());
+		System.out.println(lib.getBooks());
+		System.out.println(lib.getBooks().get(0).getAuthor());
 	}
 }
